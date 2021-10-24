244a878e8a5d817fc0568a4db6bbcd4eeac28d5f
Jakub Klimek <jakubklimek@student.agh.edu.pl>
2021-10-24 22:59:07 +0200

    3/4 work done

--- graphs_1/graphs_1.py
+++ graphs_1/graphs_1.py
@@ -13,11 +13,53 @@ def adjmat_to_adjlist(adjmat: List[List[int]]) -> Dict[int, List[int]]:
             del dictonary[start]
     return dictonary
 
+def dfs_recursive(G: Dict[int, List[int]], s: int) -> List[int]:
+    def dfs_recursive_visited(G, s, visited = None):
+        if visited is None:
+            visited = []
+        visited.append(s)
+        for u in G[s]:
+            if u not in visited:
+             dfs_recursive_visited(G, u, visited)
+        return visited
+    return dfs_recursive_visited(G, s)
+ 
+def dfs_iterative(G: Dict[int, List[int]], s: int) -> List[int]:
+    stack = []
+    visited = []
+    stack.append(s)
+    while stack:
+        looking_stack = []
+        s = stack[0]
+        stack.pop(0)
+        if s not in visited:
+            visited.append(s)
+            for u in G[s]:
+                if u in visited:
+                    stack.append(u)
+                else:
+                    looking_stack.append(u)
+                    stack = looking_stack + stack
+    return visited
+
+
 A = [
     [1, 3, 0],
-    [0, 0, 1],
+    [0, 0, 2],
     [1, 2, 0]
 ]
 
+G = {
+    1: [2, 3, 5],
+    2: [1, 4, 6],
+    3: [1, 7],
+    4: [2],
+    5: [1, 6],
+    6: [2, 5],
+    7: [3]
+}
+
 print(A)
 print(adjmat_to_adjlist(A))
+print(dfs_recursive(G, 1))
+print(dfs_iterative(G, 1))
