3107ca105627b5963204717584e6b9ed54e883f4
Kantorek <jakubklimek9@wp.pl>
2021-11-08 23:13:25 +0100

    Fin...

--- graphs_2/graphs_2_tests.py
+++ graphs_2/graphs_2_tests.py
@@ -3,17 +3,18 @@ import graphs_2 as g
 import networkx as nx
 
 
-test_graph = nx.MultiDiGraph()
-test_graph.add_weighted_edges_from([(1, 2, 0.7), (2, 3, 0.6), (2, 3, 0.5), (1, 3, 1.4)])
-min_path = g.find_min_trail(test_graph, 1, 3)
-distance = 0
-for x in range(len(min_path)):
-    distance += min_path[x].Weight
+
 
 class Test(unittest.TestCase):
 
     def test(self):
-        self.assertEqual(distance, nx.dijkstra_path_length(test_graph, 1, 3))
+        G = g.load_multigraph_from_file("directed_graph.dat")
+        trail = g.find_min_trail(G, 1, 3)
+        distance = 0
+        for x in trail:
+            distance += x.weight
+    
+        self.assertEqual(distance, nx.dijkstra_path_length(G, 1, 3))
 
 if __name__ == '__main__':
     unittest.main()
--- graphs_2/tempCodeRunnerFile.py
+++ graphs_2/tempCodeRunnerFile.py
@@ -1,9 +1,1 @@
-with open("directed_graph_blank_lines.dat", 'r') as f:
-    M_graph = nx.MultiDiGraph()
-    path = []
-    for line in f.readlines():
-        if(line.strip() != ""):
-            path.append(tuple(map(int, line.strip().split(" ")[
-                :-1]))+(float(line.strip().split(" ")[-1]),))
-M_graph.add_weighted_edges_from(path)
-print(M_graph)
+0
